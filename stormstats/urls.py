ebcac1c63e012cc52a8947565ccf8838a58dff27
pr1malbyt3s <aaronwilliams.infsosec@gmail.com>
2021-01-29 18:07:29 -0500

    Updated html pages and views

--- stormstats/urls.py
+++ stormstats/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'stormstats'
 urlpatterns = [
-    path('', views.player_list, name='index'),
+    path('', views.HomeView.as_view(), name='home'),
+    path('about/', views.AboutView.as_view(), name='about'),
+    path('roster/', views.RosterView.as_view(), name='roster'),
 ]
--- stormstats/views.py
+++ stormstats/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.views import generic
 from .models import Player
 
+class HomeView(generic.TemplateView):
+    template_name = 'stormstats/home.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "StormStats - Home"
+        context['activate'] = 'home'
+        return context
+
+class AboutView(generic.TemplateView):
+    template_name = 'stormstats/about.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "StormStats - About"
+        context['activate'] = 'about'
+        return context
+
+class RosterView(generic.TemplateView):
+    template_name = 'stormstats/roster.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = "StormStats - Roster"
+        context['activate'] = 'roster'
+        context['players'] = Player.objects.all()
+        return context
+
+'''
 def player_list(request):
     players = Player.objects.all()
     return render(request, 'stormstats/index.html', {'players': players})
+'''
