288fe11bd265d23ccaaf41d621e0c366f6312ace
pr1malbyt3s <aaronwilliams.infosec@gmail.com>
2021-04-20 09:38:38 -0400

    Update urls.py

--- ncstats/urls.py
+++ ncstats/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('stormstats.urls')),
-]
+if settings.ADMIN_ENABLED:
+    urlpatterns = [
+        path('admin/', admin.site.urls),
+        path('', include('stormstats.urls')),
+    ]
+else:
+    urlpatterns = [
+        path('', include('stormstats.urls')),
+    ]
