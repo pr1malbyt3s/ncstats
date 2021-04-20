6c428b6d68899c387149125643dc4dffc375e597
pr1malbyt3s <aaronwilliams.infosec@gmail.com>
2021-04-20 09:37:37 -0400

    Update settings.py

--- ncstats/settings.py
+++ ncstats/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+ADMIN_ENABLED = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'stormstats.ncstats.com']
 
